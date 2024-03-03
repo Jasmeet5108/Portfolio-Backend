1ccd1074f87aaed6e0e74092260e0578e6baa721
Jasmeet Singh <jasmeet5108k@gmail.com>
2024-03-03 15:32:13 +0530

    updated index.js & vercel.json

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ connectToMongo(process.env.MONGODB_URI).then(() => { console.log(`MongoDb connec
 const app = express();
 const port = process.env.PORT || 9000;
 
-app.get("/", (req, res) => {
-    res.send("Hey!!! Your backend is working fine")
-})
-
 app.use(express.json());
 app.use(cors());
 app.use("/form", userRouter);
+
+app.get("/", (req, res) => {
+    res.send("Hey!!! Your backend is working fine")
+})
 
 app.listen(port, () => { console.log(`App listening on port:${port}`) });
